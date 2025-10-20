11f3afef503d79bf8643e9a6171df610da595c99
Marianojz <marianojz@gmail.com>
2025-10-20 09:43:45 -0300

    Update AdminPanelPro.tsx

--- src/pages/AdminPanelPro.tsx
+++ src/pages/AdminPanelPro.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { 
   Users, Gavel, Package, TrendingUp, DollarSign, AlertCircle, 
   Clock, CheckCircle, XCircle, Truck, FileText, Download,
