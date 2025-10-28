e155e1033f758246985c63d28d3e4549b4e8c40b
Marianojz <marianojz@gmail.com>
2025-10-28 19:13:34 -0300

    Update useStore.ts

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -1,5 +1,5 @@
 import { ref, push, update } from 'firebase/database';
-import { realtimeDb } from '../firebase';
+import { realtimeDb } from '../config/firebase';
 import { create } from 'zustand';
 import { User, Auction, Product, CartItem, Notification, Theme, Bot, Order, OrderStatus } from '../types';
 
