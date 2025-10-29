17a6c00b150d9eed4fdf2d24f59961e982a70cdb
Marianojz <marianojz@gmail.com>
2025-10-28 21:24:21 -0300

    Update AuctionDetail.tsx

--- src/pages/AuctionDetail.tsx
+++ src/pages/AuctionDetail.tsx
@@ -307,7 +307,7 @@ const AuctionDetail = () => {
     </div>
   </div>
 )}
-              ) : (
+              )} : (
                 <div style={{ padding: '1rem', background: '#666', color: 'white', borderRadius: '0.75rem', textAlign: 'center' }}>
                   <Clock size={20} style={{ marginBottom: '0.5rem' }} />
                   <div>⏰ Subasta Finalizada</div>
